afcd4b3d23f0b511a2973e8548e861915d82e419
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-26 15:43:30 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,11 +5,18 @@ def load_library(path)
   hash = {"get_meaning" => {}, "get_emoticon" => {}}
   emoticons.each do |word, array_of_symbols|
     array_of_symbols.each do |element|
+<<<<<<< HEAD
       hash["get_meaning"][array_of_symbols[1]] = word
       hash["get_emoticon"][array_of_symbols[0]] = array_of_symbols[1]
     end
   end
 hash
+=======
+      hash["get meaning"][word] = element
+    end
+  end
+  binding.pry
+>>>>>>> d1aea1c34d2890e85196b502d78a85ff190cb2dd
 end
 
 def get_japanese_emoticon(path, emoticon)
